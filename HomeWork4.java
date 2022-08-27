2eb6f61d37c8d618b13a532b26f37ddb095fef2a
kaidou2 <111167638+kaidou2@users.noreply.github.com>
2022-08-27 22:14:59 +0900

    initial commit

---
+++ HomeWork4.java
@@ -0,0 +1,8 @@
+import java.util.List;
+public class HomeWork4 {
+    public static void main(String[] args) {
+        List<String> names = List.of("Zakoshisyoh", "Kotouge", "Nishimura", "Masanori", "Takashi");
+        names.stream().map(name -> name.getBytes()).forEach(name -> System.out.println(name));
+        names.stream().map(String::toLowerCase).forEach(System.out::println);
+    }
+}

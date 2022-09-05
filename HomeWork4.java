c6d07159b534cd7869c4a900fca63c64b68b0f42
kaidou2 <111167638+kaidou2@users.noreply.github.com>
2022-09-05 15:56:46 +0900

    Update HomeWork4.java

--- HomeWork4.java
+++ HomeWork4.java
@@ -1,7 +1,7 @@
 import java.util.List;
 public class HomeWork4 {
     public static void main(String[] args) {
-        List<String> names = List.of("Zakoshisyoh", "Kotouge", "Nishimura", "Masanori", "Takashi");
+        List<String> names = List.of("Zakoshisyoh", "Kotouge", "Nishimura", "Masanori", "Takashi", "rinse");
         names.stream().map(name -> name.getBytes()).forEach(name -> System.out.println(name));
         names.stream().map(String::toLowerCase).forEach(System.out::println);
     }
